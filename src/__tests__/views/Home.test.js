15d89ff841ed5da852b0a4959a38f3304422cfae
Arthur <arthurkaplan@gmx.at>
2022-04-01 15:09:47 +0200

    Added test for Home when adding text and clicking button

--- src/__tests__/views/Home.test.js
+++ src/__tests__/views/Home.test.js
@@ -6,16 +6,30 @@ import Home from "../../views/Home";
 describe("When a user interacts with the Input field", () => {
   it("should return 'abc' for the react state if the user types 'abc'", () => {
     // Arrange / Given
-
     render(<Home />);
+    const expected = "abc";
+    const input = screen.getByLabelText("Fruit Name");
 
     // Act / When
-
-    const expected = "";
-
-    fireEvent.change(screen.getByLabelText("Fruit Name"), "a");
+    fireEvent.change(input, { target: { value: "abc" } });
 
     // Assert / Then
-    expect(actual).toBe(expected);
+    expect(input.value).toBe(expected);
+  });
+
+  it("should add a new text 'abc' when the user adds abs in the input and clicks the button", () => {
+    // Arrange / Given
+    render(<Home />);
+    const expected = "abc";
+    const input = screen.getByLabelText("Fruit Name");
+    const button = screen.getByText("Create New Fruit");
+
+    // Act / When
+    fireEvent.change(input, { target: { value: "abc" } });
+    fireEvent.click(button);
+    const actual = screen.getByText("abc");
+
+    // Assert / Then
+    expect(actual).toHaveTextContent(expected);
   });
 });
