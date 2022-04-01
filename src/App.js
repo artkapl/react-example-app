e7cf665fe75138db69b947353e76eec504094ce0
Arthur <arthurkaplan@gmx.at>
2022-04-01 14:47:14 +0200

    Added Page to App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./views/Home";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Error404 from "./views/Error404";
+import Page from "./components/Page";
 
 function App() {
   return (
+    <Page>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
+    </Page>
   );
 }
 
