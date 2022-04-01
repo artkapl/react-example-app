f3eb80882cdb0244d81c99d175e8de74f0fed0c3
Arthur <arthurkaplan@gmx.at>
2022-04-01 14:50:55 +0200

    Formatted Error404.js

--- src/views/Error404.js
+++ src/views/Error404.js
@@ -2,11 +2,7 @@ import Navigation from "../components/Navigation";
 import Page from "../components/Page";
 
 function Error404() {
-    return (
-        <>
-            404: Not Found!
-        </>
-    );
+  return <>404: Not Found!</>;
 }
 
 export default Error404;
