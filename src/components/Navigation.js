2723a2bad713f25fc9d572930da275c2be21432d
Arthur <arthurkaplan@gmx.at>
2022-04-01 15:12:22 +0200

    Formatted Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,25 +5,26 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 
 function Navigation() {
-    return (
-        <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand as={Link} to="/">Mein Cooler Webshop</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-      <Nav.Link as={Link} to="/">
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Mein Cooler Webshop
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
               Home
             </Nav.Link>
             <Nav.Link as={Link} to="/login">
               Login
             </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-    );
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navigation;
